d90e799451f13dc05fadb3bf791c6d3b1ebfb350
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-14 12:32:13 +0200

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script >
+import AppMain from './components/AppMain.vue';
+
+export default{
+  data(){
+      return {
+          
+      }
+  },
+  components: {
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss';
+@use './styles/partials/variables' as *;
+@use './styles/partials/mixins' as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,20 @@
+<script>
+import axios from 'axios';
+import { store } from "../store.js";
+
+export default{
+    data(){
+        return {
+            message: 'Main'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1> {{ message }} </h1>
+</template>
+
+<style scoped>
+
+</style>
