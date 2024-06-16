bd98df870e5276537b01d96280f0c8f200a34105
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-16 12:01:00 +0200

    added language icons

--- src/components/MainSingleCard.vue
+++ src/components/MainSingleCard.vue
@@ -23,7 +23,10 @@ export default{
             <div class="card-body">
                 <h5 class="card-title">{{ singleCard.title }} {{ singleCard.name }}</h5>
                 <h6 class="card-subtitle mb-2 text-body-secondary">{{ singleCard.original_title }} {{ singleCard.original_name }}</h6>
-                <p class="card-text fs-6">{{ singleCard.original_language }}</p>
+                <p class="lang-icon mb-0" :class="`lang-icon-${singleCard.original_language}`" </p>
+
+
+                <!-- <p class="card-text fs-6">{{ singleCard.original_language }}</p> -->
             </div>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">{{ singleCard.vote_average }}</li>
@@ -33,5 +36,9 @@ export default{
 </template>
 
 <style scoped lang="scss">
+@use '../../node_modules/@textabledev/langs-flags-list/lang-flags.css';
 
+.lang-icon {
+    background-image: url(../../node_modules/@textabledev/langs-flags-list/lang-flags.png);
+}
 </style>
