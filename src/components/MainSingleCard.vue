b168e77e2ef1d15b35fefcde6358567202f5d103
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-16 12:34:02 +0200

    added poster images to cards

--- src/components/MainSingleCard.vue
+++ src/components/MainSingleCard.vue
@@ -18,8 +18,11 @@ export default{
 
 <template>
     <article class="col-3 mb-3">
-        <div class="card">
-            <!-- <img src="..." class="card-img-top" alt="..."> -->
+        <div class="card h-100">
+            <div class="card-image">
+                <p v-if="singleCard.poster_path == null"> {{ singleCard.title }} {{ singleCard.name }} has no poster</p>
+                <img v-else :src="`https://image.tmdb.org/t/p/w342/${singleCard.poster_path}`" class="card-img-top h-100" :alt="`${singleCard.title}${singleCard.name} Poster`">
+            </div>
             <div class="card-body">
                 <h5 class="card-title">{{ singleCard.title }} {{ singleCard.name }}</h5>
                 <h6 class="card-subtitle mb-2 text-body-secondary">{{ singleCard.original_title }} {{ singleCard.original_name }}</h6>
@@ -37,6 +40,15 @@ export default{
 
 <style scoped lang="scss">
 @use '../../node_modules/@textabledev/langs-flags-list/lang-flags.css';
+
+.card-image {
+    height: 389.5px;
+
+    p{
+        text-align: center;
+        margin-top: 1rem;
+    }
+}
 
 .lang-icon {
     background-image: url(../../node_modules/@textabledev/langs-flags-list/lang-flags.png);
