ae13fa7dea102ffe319d492df5068b09d3aee681
lazzaric92 <lazzaric_92@hotmail.it>
2024-06-16 14:50:58 +0200

    added font-awesome icons

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from '../node_modules/@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '../node_modules/@fortawesome/vue-fontawesome';
+import { faStar as fasFaStar } from "../node_modules/@fortawesome/free-solid-svg-icons";
+import { faStar as farFaStar } from "../node_modules/@fortawesome/free-regular-svg-icons";
 
-createApp(App).mount('#app')
+library.add(fasFaStar, farFaStar);
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
