f1e18cb934200ca484f8519bb3065c1a12c285fc
lazzaric92 <lazzaric_92@hotmail.it>
2024-08-28 20:33:42 +0200

    added pages with api calls

---
+++ src/components/carousels/DiscoverTv.vue
@@ -0,0 +1,38 @@
+<script>
+export default{
+    data(){
+        return {
+            discoverArray: [],
+        }
+    },
+    methods: {
+        getDiscoverList: function(){
+            axios.get('https://api.themoviedb.org/3/discover/tv', {
+                params: {
+                api_key: "861729733fec3d9d72d05bb5c85381e2",
+                language: "it-IT",
+                }
+            })
+            .then((response) => {
+                // lista film
+                this.discoverArray = response.data.results;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });            
+        },
+    },
+    created(){
+        this.getDiscoverList();
+    }
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/carousels/TrendingMovies.vue
@@ -0,0 +1,38 @@
+<script>
+export default{
+    data(){
+        return {
+            trendingArray: [],
+        }
+    },
+    methods: {
+        getTrendingList: function(){
+            axios.get('https://api.themoviedb.org/3/trending/movies/week', {
+                params: {
+                api_key: "861729733fec3d9d72d05bb5c85381e2",
+                language: "it-IT"
+                }
+            })
+            .then((response) => {
+                // lista film
+                this.trendingArray = response.data.results;
+                // console.log(this.store.moviesList);
+            })
+            .catch(function (error) {
+                console.log(error);
+            }); 
+        }
+    },
+    created(){
+        this.getTrendingList();
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped  lang="scss">
+
+</style>
