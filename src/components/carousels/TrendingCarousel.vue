a77825ec5fd3821eaa87758dd56630337fa867a9
lazzaric92 <lazzaric_92@hotmail.it>
2024-09-02 19:43:39 +0200

    added carousel dots

--- src/components/carousels/TrendingCarousel.vue
+++ src/components/carousels/TrendingCarousel.vue
@@ -7,7 +7,7 @@ export default{
         return {
             trendingArray: [],
             carouselArray: [],
-            currentIndex: 1,
+            currentIndex: 0,
             store
         }
     },
@@ -54,12 +54,17 @@ export default{
 </script>
 
 <template>
-    <div class="col-12 d-flex justify-content-center">
+    <div class="col-12 d-flex flex-column align-items-center">
         <template v-for="(arrayObj, index) in carouselArray">
-            <article v-if="index === this.currentIndex" class="d-flex justify-content-between">
-                <div class="article-info position-relative z-2 w-25 py-4 px-3 d-flex flex-column justify-content-around align-items-center">
-                    <h1 class="fs-3 text-white text-center">{{ (arrayObj.media_type === 'movie') ? arrayObj.title : arrayObj.name }}</h1>
-                    <button class="btn btn-lg btn-outline-light">Maggiori informazioni </button>
+            <article v-if="index === this.currentIndex" class="d-flex justify-content-between mb-4">
+                <div class="article-info position-relative z-2 w-25 py-4 px-3 d-flex flex-column">
+                    <div class="text-center h-75 d-flex flex-column justify-content-center align-items-center">
+                        <h1 class="fs-3 text-white text-center mb-4">{{ (arrayObj.media_type === 'movie') ? arrayObj.title : arrayObj.name }}</h1>
+                        <h2 class="fs-4 text-secondary" v-if="arrayObj.title != arrayObj.original_title || arrayObj.name != arrayObj.original_name">{{ (arrayObj.media_type === 'movie') ? arrayObj.original_title : arrayObj.original_name }}</h2>
+                    </div>
+                    <div class="h-25 d-flex justify-content-center align-items-center">
+                        <button class="btn btn-lg btn-outline-light">Maggiori informazioni </button>
+                    </div>
                 </div>
                 <div class="article-img position-relative h-100 w-75 text-end">
                     <div class="dark_overlay position-absolute z-1 w-100 h-100"></div>
@@ -67,10 +72,18 @@ export default{
                 </div>
             </article>
         </template>
+        <div class="w-100 d-flex justify-content-center align-items-center pt-2">
+            <template v-for="(arrayObj, index) in carouselArray">
+                <div class="carousel-dot rounded-circle p-1 mx-3" :class="(index === this.currentIndex) ? 'bg_active' : 'bg-white'" @click="this.currentIndex = index">
+                </div>
+            </template>
+        </div>
     </div>
 </template>
 
 <style scoped lang="scss">
+@use '../../styles/partials/variables' as *;
+
     article{
         width: 90%;
         height: 600px;
@@ -86,5 +99,13 @@ export default{
         .article-img{
             overflow-x: clip;
         }
+    }
+
+    .bg_active{
+        background-color: $my_logo-color;
+    }
+
+    .carousel-dot {
+        cursor: pointer;
     }
 </style>
