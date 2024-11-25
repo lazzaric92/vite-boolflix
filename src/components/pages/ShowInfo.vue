110c137e365e8605960b1d7bad2d680ee10cf023
lazzaric92 <lazzaric_92@hotmail.it>
2024-11-25 11:16:23 +0100

    created function to get product info

--- src/components/pages/ShowInfo.vue
+++ src/components/pages/ShowInfo.vue
@@ -1,23 +1,45 @@
 <script>
+import axios from 'axios';
+
 export default{
     data(){
         return {
-            message: 'Info'
+            itemInfo: [],
         }
     },
     methods: {
+        getInfo(){
+            axios.get(`https://api.themoviedb.org/3/${this.$route.params.media}/${this.$route.params.id}`, {
+                params: {
+                api_key: "861729733fec3d9d72d05bb5c85381e2",
+                language: "it-IT",
+                }
+            })
+            .then((response) => {
+                // console.log(response.data);
+                this.itemInfo = response.data;
+                console.log(this.itemInfo);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+            .finally(function(){
+                //
+            }); 
+        },
         printInfo(){
             // console.log(this.$route.params.id, this.$route.params.media);
-        }
+        },
     },
     created(){
         this.printInfo();
+        this.getInfo();
     }
 }
 </script>
 
 <template>
-    <h1> {{ message }} </h1>
+    
 </template>
 
 <style scoped lang="scss">
